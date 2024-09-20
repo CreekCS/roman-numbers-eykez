517365063b616d848ca3fb4f662302287445187b
Elynor Sykes <esykes@cherrycreekschools.org>
2024-09-20 21:30:02 +0000

    working code? RomansIIRunner changed to RomansRunner

--- Romans.java
+++ Romans.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elynor Sykes
 
 import java.util.*;
 
@@ -12,12 +12,26 @@ public class Romans
 	{
 		m = new TreeMap<String, Integer>();
 		
-		//complete your map				
+		//complete your map		
+		m.put("I", 1);
+        m.put("V", 5);
+        m.put("X", 10);
+		m.put("L",50);	
+		m.put("C",100);
+		m.put("D",500);
+		m.put("M",1000);
+
 	}
 	
 	public int getNumber( String s )
 	{
 		//complete this method
+		if (m.containsKey(s)) {
+            return m.get(s);
+        }
+        return 0; // Return 0 for invalid input
+
+
 	}
 
 }
